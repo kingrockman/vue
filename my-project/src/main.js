b0c02009c6c610e04a1510b1980c296535a70596
kingrockman <181013906@qq.com>
2019-10-08 17:54:34 +0800

    err

---
+++ my-project/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue(App)
+app.$mount()
+
+export default {
+  config: {
+    pages: [
+      'pages/index/main',
+      'pages/logs/main'
+    ],
+    window: {
+      'backgroundTextStyle': 'light',
+      'navigationBarBackgroundColor': '#fff',
+      'navigationBarTitleText': 'WeChat',
+      'navigationBarTextStyle': 'black'
+    }
+  }
+}
---
+++ my-project/src/pages/todolist/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <p></p>
+</div>
+
+</template>
+
+<script>
+
+import Todolist from '@/components/Todolist'
+
+export default {
+  components: {
+    Todolist
+  }
+
+}
+</script>
+<style>
+</style>
